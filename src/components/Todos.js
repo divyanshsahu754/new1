0007751f3f735b8d89e923c0170a3168cace6275
divyanshsahu754 <divyanshsahu754@gmail.com>
2023-06-08 13:23:04 +0530

    Try Redux React With EDit

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Icon } from 'react-icons-kit';
 import {trash} from 'react-icons-kit/feather/trash'
 import {edit2} from 'react-icons-kit/feather/edit2'
-import { removeTodo, handleCheckbox } from '../redux/todoapp/actions';
+import { removeTodo} from '../redux/todoapp/actions';
 
 export const Todos = ({handleEditClick, editFormVisibility}) => {
   // dispatch function to dispatch an action
@@ -14,10 +14,7 @@ export const Todos = ({handleEditClick, editFormVisibility}) => {
   return todos.map((todo)=>(
     <div key={todo.id} className='todo-box'>
         <div className='content'>
-            {editFormVisibility===false&&(
-              <input type="checkbox" checked={todo.completed}
-              onChange={()=>dispatch(handleCheckbox(todo.id))}></input>
-            )}
+            
             <p style={todo.completed===true?{textDecoration:'line-through'}:{textDecoration:'none'}}>
                 {todo.todo}
             </p>
--- src/redux/todoapp/reducers/operations.js
+++ src/redux/todoapp/reducers/operations.js
@@ -1,10 +1,8 @@
 /* eslint-disable array-callback-return */
-import { ADD_TODO, DELETE_ALL, REMOVE_TODO, UPDATE_CHECKBOX, UPDATE_TODO } from "../actions";
+import { ADD_TODO, DELETE_ALL, REMOVE_TODO, UPDATE_TODO } from "../actions";
 
 const initialState=[
-    {id: 1, todo: 'Buy Laptop', completed: false},
-    {id: 2, todo: 'Master Redux', completed: false},
-    {id: 3, todo: 'Watering Plants', completed: true},
+    
 ];
 
 export const operationsReducer=(state=initialState, action)=>{
@@ -23,20 +21,20 @@ export const operationsReducer=(state=initialState, action)=>{
                 if(item.id===data.id){
                     item.id = data.id;
                     item.todo = data.todo;
-                    item.completed = data.completed;
+                    
                 }
                 updatedArray.push(item);
             })
             return updatedArray;
-        case UPDATE_CHECKBOX:
-            let todoArray=[];
-            state.map((item)=>{
-                if(item.id===action.payload){
-                    item.completed = !item.completed;
-                }
-                todoArray.push(item);
-            })
-            return todoArray;
+        // case UPDATE_CHECKBOX:
+        //     let todoArray=[];
+        //     state.map((item)=>{
+        //         if(item.id===action.payload){
+        //             item.completed = !item.completed;
+        //         }
+        //         todoArray.push(item);
+        //     })
+        //     return todoArray;
         default: return state;
     }
 }
